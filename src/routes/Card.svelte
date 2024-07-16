f9a9463107397b895a38649238002ba1e36b1c28
Jai Bellare <jaibellare@gmail.com>
2024-07-17 02:42:41 +0530

    add Card

---
+++ src/routes/Card.svelte
@@ -0,0 +1,49 @@
+<script>
+    export let src;
+
+    let radius = '1em';
+</script>
+
+<div class="outer" style="--radius: {radius}">
+    <img {src} alt="alt" style="--radius: {radius}" />
+
+    <div class="inner">
+        <h3>
+            <slot name="title">Title Slot</slot>
+        </h3>
+        <p>
+            <slot name="info">More Info</slot>
+        </p>
+    </div>
+    <!-- TODO link -->
+</div>
+
+<style>
+    .outer {
+        background-color: rgb(25, 25, 25);
+        border-radius: var(--radius);
+        margin: 0.4em;
+        padding: 0em;
+        width: 15em;
+        color: #d6d6d6;
+    }
+
+    .inner {
+        padding: .7em;
+    }
+
+    img {
+        width: 15em;
+        border-radius: var(--radius) var(--radius) 0em 0em;
+    }
+
+    h3 {
+        padding: 0em;
+        margin: 0em;
+    }
+
+    p {
+        padding: 0em;
+        margin: 0em;
+    }
+</style>
---
+++ src/routes/Projects.svelte
@@ -0,0 +1,20 @@
+<script>
+    import Card from './Card.svelte';
+    import softbody from '$lib/assets/softbody.gif'
+</script>
+
+<p>
+    <!-- <Card src="$lib/assets/softbody.gif"><span slot="title">Graphics Engine</span></Card>
+    <Card src="$lib/assets/softbody.gif"><span slot="title">Graphics Engine</span></Card>
+    <Card src="$lib/assets/softbody.gif"><span slot="title">Graphics Engine</span></Card>
+    <Card src="$lib/assets/softbody.gif"><span slot="title">Graphics Engine</span></Card> -->
+    <Card src={softbody}><span slot="title">Simulating Softbodies</span></Card>
+    <!-- <Card>Tracked RC robot</Card> -->
+</p>
+
+<style>
+    p {
+        display: flex;
+        /* TODO flex stuff */
+    }
+</style>
