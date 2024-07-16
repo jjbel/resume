55606d0c03d4d43298650a70d67367761b718846
Jai Bellare <jaibellare@gmail.com>
2024-07-17 04:00:40 +0530

    projects: add softbody, hilbert_curve

--- src/routes/Card.svelte
+++ src/routes/Card.svelte
@@ -1,21 +1,24 @@
 <script>
     export let src;
+    export let link;
 
     let radius = '1em';
 </script>
 
+<!-- <div class="outer" style="--radius: {radius}; cursor: pointer;" on:click={onClick}> -->
 <div class="outer" style="--radius: {radius}">
-    <img {src} alt="alt" style="--radius: {radius}" />
+    <a href="/{link}">
+        <img {src} alt="alt" style="--radius: {radius}" />
 
-    <div class="inner">
-        <h3>
-            <slot name="title">Title Slot</slot>
-        </h3>
-        <p>
-            <slot name="info">More Info</slot>
-        </p>
-    </div>
-    <!-- TODO link -->
+        <div class="inner">
+            <h3>
+                <slot name="title">Title Slot</slot>
+            </h3>
+            <p>
+                <slot name="info">More Info</slot>
+            </p>
+        </div>
+    </a>
 </div>
 
 <style>
@@ -26,10 +29,17 @@
         padding: 0em;
         width: 15em;
         color: #d6d6d6;
+
+        transition-property: transform;
+        transition-duration: 0.2s;
+    }
+
+    .outer:hover {
+        transform: scale(1.03);
     }
 
     .inner {
-        padding: .7em;
+        padding: 0.7em;
     }
 
     img {
@@ -45,5 +55,10 @@
     p {
         padding: 0em;
         margin: 0em;
+    }
+
+    a {
+        color: inherit; /* blue colors for links too */
+        text-decoration: inherit; /* no underline */
     }
 </style>
--- src/routes/Projects.svelte
+++ src/routes/Projects.svelte
@@ -1,14 +1,19 @@
 <script>
     import Card from './Card.svelte';
-    import softbody from '$lib/assets/softbody.gif'
+    import softbody from '$lib/assets/softbody.gif';
+    import hilbert_curve from '$lib/assets/hilbert_curve.gif';
 </script>
 
 <p>
-    <!-- <Card src="$lib/assets/softbody.gif"><span slot="title">Graphics Engine</span></Card>
-    <Card src="$lib/assets/softbody.gif"><span slot="title">Graphics Engine</span></Card>
-    <Card src="$lib/assets/softbody.gif"><span slot="title">Graphics Engine</span></Card>
-    <Card src="$lib/assets/softbody.gif"><span slot="title">Graphics Engine</span></Card> -->
-    <Card src={softbody}><span slot="title">Simulating Softbodies</span></Card>
+    <Card src={softbody} link="softbody">
+        <span slot="title">Simulating Softbodies</span>
+        <span slot="info">in Samarium</span>
+    </Card>
+
+    <Card src={hilbert_curve} link="hilbert_curve">
+        <span slot="title">Animating the Hilbert Curve</span>
+        <span slot="info">in Samarium</span>
+    </Card>
     <!-- <Card>Tracked RC robot</Card> -->
 </p>
 
