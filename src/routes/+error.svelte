41c0c7a54ff697386cba9cab04ae4ae0c8086ef2
Jai Bellare <jaibellare@gmail.com>
2024-07-17 02:42:35 +0530

    add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,19 @@
+<h2>😪 Oops! An error occured.</h2>
+
+<h3><a href="/">Go back to the home page.</a></h3>
+
+<style>
+    h2,
+    h3 {
+        margin: 1em auto;
+        text-align: center;
+    }
+
+    h2 {
+        font-weight: 400;
+    }
+
+    h3 {
+        font-weight: 300;
+    }
+</style>
